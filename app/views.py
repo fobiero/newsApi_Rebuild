2eab269cac2a669ef7a8a9f0f3715e8df28bff50
feLycks <felix.obiero@student.moringaschool.com>
2022-02-03 13:44:08 +0300

    debug Routing

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+# from flask import render_template
 from app import app
 # from .request import get_news
 # from .request import get_news,get_news
@@ -7,10 +7,36 @@ from flask import render_template,request,redirect,url_for
 #views
 # @TODO: Route to homePage 
 @app.route('/')
-def home():
-
+def index():
     '''
     returns to homePage
     '''
 
-    return render_template('home.html')
+    return render_template('index.html')
+
+# @TODO: Politics Route 
+@app.route('/politics/<int:id>')
+def politics():
+    '''
+    returns to homePage
+    '''
+
+    return render_template('politics.html', id=id)
+
+# @TODO: technology Route 
+@app.route('/technology')
+def technology():
+    '''
+    returns to homePage
+    '''
+
+    return render_template('technology.html')
+
+# @TODO: Sports Route 
+@app.route('/sports')
+def sports():
+    '''
+    returns to homePage
+    '''
+
+    return render_template('sports.html')
